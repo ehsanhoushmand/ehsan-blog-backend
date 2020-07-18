7f0a9c53c86f95373fe1c7c2a6c053199c59ac01
Ehsan Houshmand <houshmand2007@gmail.com>
2020-07-18 18:54:45 +0430

    update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -26,25 +26,27 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
+	/**
+	 * Report or log an exception.
+	 *
+	 * @param Throwable $exception
+	 * @return void
+	 * @throws \Exception
+	 */
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
+	/**
+	 * Render an exception into an HTTP response.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param Throwable $exception
+	 * @return \Illuminate\Http\Response
+	 * @throws Throwable
+	 */
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
